4a30ded95b76e61c17f6eb54b1549acba07b4a9c
Stephen Brett <S00173498@mail.itsligo.ie>
2019-03-25 17:37:52 +0000

    post working with topic id

--- FrontEnd/Models/PostModel.cs
+++ FrontEnd/Models/PostModel.cs
@@ -8,8 +8,8 @@ namespace FrontEnd.Models
     public class PostModel
     {
         public string Id { get; set; }
-        public string topicID { get; set; }
+        public int topicId { get; set; }
         public string text { get; set; }
-        public int BoardID { get; set; }
+       // public int BoardID { get; set; }
     }
 }
